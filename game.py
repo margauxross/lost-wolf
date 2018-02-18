817ca71bf87e0668fb8b9829afb557319080eff7
bencallaghan <d.benjamin.callaghan@gmail.com>
2018-02-18 17:41:57 -0400

    added enemies.py and game.py

---
+++ game.py
@@ -0,0 +1,10 @@
+## Tutorial stage for the game
+
+print("You are in a dark room.")
+print("You can go north, south, and west.")
+print("You hear a gurgling in the distance.")
+
+print("Choose an action:\n")
+action_input = input('Action: ')
+
+print("You cannot do that:{}", action_input)
