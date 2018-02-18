c7fc8ea1d10a9c97c2d9e111eec1971e2afc38f0
bencallaghan <d.benjamin.callaghan@gmail.com>
2018-02-18 19:44:56 -0400

    updated enemies

--- enemies.py
+++ enemies.py
@@ -6,3 +6,13 @@ class Enemy:
 
     def is_alive(self):
         return self.hp > 0
+
+
+class GiantSpider(Enemy):
+    def __init__(self):
+        super().__init__(name="Giant Spider", hp=10, damage=2)
+
+
+class Ogre(Enemy):
+    def __init__(self):
+        super().__init__(name="Ogre", hp=30, damage=15)
