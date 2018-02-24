b600fd7889904b5d894d82909d88925dc0c045e9
root <root@LENOVO-PC.localdomain>
2018-02-24 17:33:06 -0400

    added test file

---
+++ test.py
@@ -0,0 +1,14 @@
+
+x = 4 #This is an int
+y = "garbage" # This is a string
+z = False # This is a boolean
+
+
+
+while(True):
+	print("We are on the ", x - 3 ,"th iteration")
+	print(x)
+	x = x + 1
+
+
+
