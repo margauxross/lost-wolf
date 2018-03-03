9f336dcbaffb78a69574b340e6ca06305dbcc0e2
d.benjamin.callaghan@gmail.com <root@LENOVO-PC.localdomain>
2018-03-02 20:43:43 -0400

    Dice, enemies

---
+++ dice.py
@@ -0,0 +1,17 @@
+import random
+
+class Roll():
+    def d4():
+        return random.randint(1,4)
+
+    def d6():
+        return random.randint(1,6)
+
+    def d10():
+        return random.randint(1,10)
+
+    def d8():
+        return random.randint(1,8)
+
+    def d20():
+        return random.randint(1,20)
