620b23598823ff84c9a77b79cc9de2fe0b2acce2
d.benjamin.callaghan@gmail.com <root@LENOVO-PC.localdomain>
2018-03-02 20:56:22 -0400

    enemies

--- enemies.py
+++ enemies.py
@@ -1,3 +1,5 @@
+import random
+
 class Enemy:
     def __init__(self, name, hp, damage):
         self.name = name
@@ -7,12 +9,46 @@ class Enemy:
     def is_alive(self):
         return self.hp > 0
 
+    def speak(self):
+        print("Grrrrrr")
+
 
 class GiantSpider(Enemy):
     def __init__(self):
         super().__init__(name="Giant Spider", hp=10, damage=2)
 
+    #def:
 
 class Ogre(Enemy):
     def __init__(self):
         super().__init__(name="Ogre", hp=30, damage=15)
+
+
+# Goblinoids
+
+class Goblinoid(Enemy):
+
+    def speak(self):
+        action = ["stomp", "bite", "break", "munch", "kick", "headbutt", "tickle"
+                  "reorganize", "steal", "softly caress", "punch holes in", "tear off"]
+        bodypart = ["legsticks", "thighbones", "earlobes", "eyeballs", "face", "butt",
+                    "cheeks", "belly", "tongue", "toes", "hair", "bone-jelly"]
+
+        a = random.choice(list(action))
+        b = random.choice(list(bodypart))
+
+        print("I'm gonna {0} your {1}!".format(a,b))
+
+class Goblin(Goblinoid):
+    def __init__(self):
+        super().__init__(name="Goblin", hp=10, damage=3)
+
+class LargeGoblin(Goblinoid):
+    def __init__(self):
+        super().__init__(name="Large Goblin", hp=14, damage=4)
+
+class FatGoblin(Goblinoid):
+    def __init__(self):
+        super().__init__(name="Fat Goblin", hp=24, damage=2)
+
+
--- items.py
+++ items.py
@@ -7,3 +7,15 @@ class Item():
  
     def __str__(self):
         return "{}\n=====\n{}\nValue: {}\n".format(self.name, self.description, self.value)
+
+
+class Flashlight(Item):
+    def __init__(self, name, description, value):
+        self.name = "Flashlight"
+        self.description = "A rusty antique, emits a steady, glowing beam."
+        self.value = 1
+
+    def use(self):
+        #Turn on flashlight, turn off flashlight, illuminate dark areas when turned on
+        print("the flashlight flickers but does nothing as this feature is not implemented")
+
