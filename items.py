7dab0dd6bc7bc1d2fdce9201978d964241e6daf2
margauxross <margauxaross@gmail.com>
2018-03-02 16:53:12 -0800

    merge resolve

--- items.py
+++ items.py
@@ -7,3 +7,12 @@ class Item():
  
     def __str__(self):
         return "{}\n=====\n{}\nValue: {}\n".format(self.name, self.description, self.value)
+
+
+class Weapon(Item):
+    def __init__(self, name, description, value, damage):
+        self.damage = damage
+        super().__init__(name, description, value)
+
+    def __str__(self):
+        return "{}\n=====\n{}\nValue: {}\nDamage: {}".format(self.name, self.description, self.value, self.damage)
