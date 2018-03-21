589952740474116bb9c45fde7d5202191c0784f2
d.benjamin.callaghan@gmail.com <root@LENOVO-PC.localdomain>
2018-03-21 10:18:17 -0300

    added some room subclasses

--- tiles.py
+++ tiles.py
@@ -8,7 +8,6 @@ class MapTile:
     def intro_text(self):
         raise NotImplementedError()
 
-
     def modify_player(self, player):
         raise NotImplementedError()
 
@@ -24,7 +23,6 @@ class StartingRoom(MapTile):
         # Room has no action on player
         pass
  
-
 class LootRoom(MapTile):
     def __init__(self, x, y, item):
         self.item = item
@@ -46,3 +44,41 @@ class EnemyRoom(MapTile):
         if self.enemy.is_alive():
             the_player.hp = the_player.hp - self.enemy.damage
             print("Enemy does {} damage. You have {} HP remaining.".format(self.enemy.damage, the_player.hp))
+
+
+class EmptyCavePath(MapTile):
+    def intro_text(self):
+        return """
+        Another unremarkable part of the cave. You must forge onwards.
+        """
+
+    def modify_player(self, player):
+        # Room has no action on player
+        pass
+
+
+class GiantSpiderRoom(EnemyRoom):
+    def __init__(self, x, y):
+        super().__init__(x, y, enemies.GiantSpider())
+
+    def intro_text(self):
+        if self.enemy.is_alive():
+            return """
+            A giant spider jumps down from its web in front of you!
+            """
+        else:
+            return """
+            The corpse of a dead spider rots on the ground.
+            """
+
+
+class FindDaggerRoom(LootRoom):
+    def __init__(self, x, y):
+        super().__init__(x, y, items.Dagger())
+
+    def intro_text(self):
+        return """
+        Your notice something shiny in the corner.
+        It's a dagger! You pick it up.
+        """
+
