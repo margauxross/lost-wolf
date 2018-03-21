df72d3332a2e7f981865b63c558ba7719fa7a054
d.benjamin.callaghan@gmail.com <root@LENOVO-PC.localdomain>
2018-03-21 10:26:58 -0300

    added some item classes

--- items.py
+++ items.py
@@ -9,6 +9,14 @@ class Item():
         return "{}\n=====\n{}\nValue: {}\n".format(self.name, self.description, self.value)
 
 
+class Gold(Item):
+    def __init__(self, amt):
+        self.amt = amt
+        super().__init__(name="Gold",
+                         description="A round coin with {} stamped on the front.".format(str(self.amt)),
+                         value=self.amt)
+
+
 class Flashlight(Item):
     def __init__(self):
         super().__init__(
@@ -28,6 +36,8 @@ class Flashlight(Item):
     def __str__(self):
         return "{}\n=====\n{}\nValue: {}\nDamage: {}".format(self.name, self.description, self.value, self.damage)
 
+
+
 class Stick(Weapon):
     def __init__ (self):
         super().__init__(
@@ -39,3 +49,17 @@ class Stick(Weapon):
     def use(self):
         #Poke with stick, throw stick, drop stick, burn stick
         print ("The stick pokes! It causes dubious damage!")
+
+class Rock(Weapon):
+    def __init__(self):
+        super().__init__(name="Rock",
+                         description="A fist-sized rock, suitable for bludgeoning.",
+                         value=0,
+                         damage=5)
+
+class Dagger(Weapon):
+    def __init__(self):
+        super().__init__(name="Dagger",
+                         description="A small dagger with some rust. Somewhat more dangerous than a rock.",
+                         value=10,
+                         damage=10)
